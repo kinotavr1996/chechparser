bde1b2604e5e98c99452997d5b61d1d490e867ab
kinotavr1996 <olegolegovich1996@gmail.com>
2018-07-31 22:46:36 +0300

    Added form

--- ParsingSystem/ParsingSystem/MainWindow.xaml.cs
+++ ParsingSystem/ParsingSystem/MainWindow.xaml.cs
@@ -31,7 +31,42 @@ namespace ParsingSystem
 		{
 			OpenFileDialog openFileDialog = new OpenFileDialog();
 			if (openFileDialog.ShowDialog() == true)
-				txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+				txtEditorBrowse.Text = File.ReadAllText(openFileDialog.FileName);
+		}
+
+		private void btnSaveFile_Click(object sender, RoutedEventArgs e)
+		{
+			var dlg = new SaveFileDialog
+			{
+				FileName = "Result", // Default file name
+				DefaultExt = ".text", // Default file extension
+				Filter = "Text documents (.txt)|*.txt" // Filter files by extension
+			};
+
+			// Show save file dialog box
+			Nullable<bool> result = dlg.ShowDialog();
+
+			// Process save file dialog box results
+			if (result == true)
+			{
+				// Save document
+				string filename = dlg.FileName;
+			}
+		}
+
+		private void btnConfigure_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnScan_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnSettings_Click(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
